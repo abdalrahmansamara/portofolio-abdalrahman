71e03d8a78c32942a49b04e834b7507159ec6cb6
abdalrahmansamara <abdsamara_1998@yahoo.com>
2021-04-22 22:06:00 +0300

    porto2

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const PORT = process.env.PORT || 3000;
 
 server.use(express.static('./public'));
 
+server.get('/welcome', (req,res) => {
+  res.send('Welcome to our website');
+})
+server.get('/tel', (req,res) => {
+  res.send('THIS IS PRIVATE!!!!!!');
+})
 server.listen(PORT, () => {
   console.log(`Listening to Port ${PORT}`)
 })
